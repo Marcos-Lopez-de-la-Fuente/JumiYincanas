f3feefd54820e87d720d1228c9c798001f586163
Marcos <mlopez@cifpfbmoll.eu>
2022-10-20 00:51:54 +0200

    Add editorconfig

--- layouts/explicacionesjumi/primeraexplicacion/JFrameVsJWindow.java
+++ layouts/explicacionesjumi/primeraexplicacion/JFrameVsJWindow.java
@@ -6,12 +6,12 @@ public class JFrameVsJWindow {
     public JFrameVsJWindow() {
         JFrame jFrame = new JFrame("Esto es un JFrame");
         jFrame.setSize(350, 300);
-        jFrame.setLocation(100,100);
+        jFrame.setLocation(100, 100);
         jFrame.setVisible(true);
 
         JWindow jWindow = new JWindow();
         jWindow.setSize(350, 300);
-        jWindow.setLocation(500,100);
+        jWindow.setLocation(500, 100);
         jWindow.setVisible(true);
     }
 }
