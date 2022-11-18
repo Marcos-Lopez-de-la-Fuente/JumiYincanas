92f0db2b87e56f1fa2dcb0e040932250252d9cda
Marcos <marcosldlf@gmail.com>
2022-11-18 04:44:59 +0100

    Torres de Hanoi

--- recursividad/TorresDeHanoi.java
+++ recursividad/TorresDeHanoi.java
@@ -2,16 +2,20 @@ package recursividad;
 
 public class TorresDeHanoi {
 
-    public static void torreHanoi(int n, int origen, int auxiliar, int destino) {
-        if (n > 0) {
-            torreHanoi(n - 1, origen, auxiliar, destino);
-            System.out.println("Se mueve anillo desde torre " + origen + " hasta torre " + destino);
-            torreHanoi(n - 1, auxiliar, destino, origen);
+    static void torreHanoi(int n, char from_rod, char to_rod, char helper_rod) {
+        if (n == 1) {
+            System.out.println("1: Take disk 1 from rod " + from_rod + " to rod " + to_rod);
+        } else {
+            torreHanoi(n - 1, from_rod, helper_rod, to_rod);
+            System.out.println("2: Take disk " + n + " from rod " + from_rod + " to rod " + to_rod);
+            torreHanoi(n - 1, helper_rod, to_rod, from_rod);
         }
-
     }
 
-    public static void main(String[] args) {
-        torreHanoi(3, 1, 2, 3);
+
+    public static void main(String args[]) {
+        int n = 3;
+        torreHanoi(n, 'A', 'C', 'B');
+        // torre(3, 'A', 'B', 'C');
     }
 }
